82cd7d6bceceeb150936c2ef0e91f09afde51d81
sebastianLedzianowski <136706736+sebastianLedzianowski@users.noreply.github.com>
2024-01-31 20:19:10 +0100

    Clear poetry and update birthday -> Optical[date]

--- src/schemas.py
+++ src/schemas.py
@@ -1,3 +1,6 @@
+from datetime import date
+from typing import Optional
+
 from pydantic import BaseModel, Field, EmailStr
 
 class ContactBase(BaseModel):
@@ -7,7 +10,7 @@ class ContactBase(BaseModel):
 class ContactResponse(ContactBase):
     name: str = Field(max_length=50)
     lastname: str = Field(max_length=50)
-    birthday: str = Field(max_length=20, example="YYYY-MM-DD")
+    birthday: Optional[date]
     id: int
     email: EmailStr | None = Field(default=None)
     phone_number: str = Field(max_length=20, example="+48-876-654-765")
