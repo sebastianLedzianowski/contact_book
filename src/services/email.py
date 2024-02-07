ecfbbb9e204ebfc188f260e32eb9addc44dc200a
sebastianLedzianowski <136706736+sebastianLedzianowski@users.noreply.github.com>
2024-02-07 19:23:07 +0100

    add confirmed_email and request_emial

--- src/services/email.py
+++ src/services/email.py
@@ -4,26 +4,26 @@ from fastapi_mail import FastMail, MessageSchema, ConnectionConfig, MessageType
 from fastapi_mail.errors import ConnectionErrors
 from pydantic import EmailStr
 
-from services.auth import auth_service
+from src.services.auth import auth_service
 
-import environ
-
-env = environ.Env(DEBUG=(bool, True), EMAIL_USE_TLS=(bool, True), EMAIL_USE_SSL=(bool, True))
-environ.Env.read_env()
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 conf = ConnectionConfig(
-    MAIL_USERNAME=env("MAIL_USERNAME"),
-    MAIL_PASSWORD=env("MAIL_PASSWORD"),
-    MAIL_FROM=EmailStr("example@example.com"),
-    MAIL_PORT=env("MAIL_PORT"),
-    MAIL_SERVER=env("MAIL_SERVER"),
-    MAIL_FROM_NAME=env("MAIL_FROM_NAME"),
-    MAIL_STARTTLS=env("MAIL_STARTTLS"),
-    MAIL_SSL_TLS=env("MAIL_SSL_TLS"),
-    USE_CREDENTIALS=env("USE_CREDENTIALS"),
-    VALIDATE_CERTS=env("VALIDATE_CERTS"),
-    TEMPLATE_FOLDER=env("TEMPLATE_FOLDER"),
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
+    MAIL_FROM="example@meta.ua",
+    MAIL_PORT=os.getenv("MAIL_PORT"),
+    MAIL_SERVER=os.getenv("MAIL_SERVER"),
+    MAIL_FROM_NAME=os.getenv("MAIL_FROM_NAME"),
+    MAIL_STARTTLS=os.getenv("MAIL_STARTTLS"),
+    MAIL_SSL_TLS=os.getenv("MAIL_SSL_TLS"),
+    USE_CREDENTIALS=os.getenv("USE_CREDENTIALS"),
+    VALIDATE_CERTS=os.getenv("VALIDATE_CERTS"),
+    TEMPLATE_FOLDER=Path(__file__).parent / 'templates',
 )
+print()
 
 async def send_email(email: EmailStr, username: str, host: str):
     try:
