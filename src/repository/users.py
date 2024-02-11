81d4ea3b2cf2773aaa0c0bd84b961321cd34edd1
sebastianLedzianowski <136706736+sebastianLedzianowski@users.noreply.github.com>
2024-02-11 17:58:28 +0100

    Removed the random avatar assignment logic

--- src/repository/users.py
+++ src/repository/users.py
@@ -10,13 +10,7 @@ async def get_user_by_email(email: str, db: Session) -> Type[User]:
     return db.query(User).filter(User.email == email).first()
 
 async def create_user(body: UserModel, db: Session) -> User:
-    avatar = None
-    try:
-        g = Gravatar(body.email)
-        avatar = g.get_image()
-    except Exception as e:
-        print(e)
-    new_user = User(**body.dict(), avatar=avatar)
+    new_user = User(**body.dict())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
