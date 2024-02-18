80bc8ec8a1adef64e40b361badf5b1247f6ca598
sebastianLedzianowski <136706736+sebastianLedzianowski@users.noreply.github.com>
2024-02-18 17:33:59 +0100

    Add test_routes_users.py

---
+++ tests/test_routes_users.py
@@ -0,0 +1,33 @@
+from src.database.models import User
+from tests.test_routes_auth import login_user_token_created
+from src.routes.users import rate_limit
+
+
+def test_read_users_me(client, user, session, monkeypatch):
+    login_user_token_created(client, session, user, monkeypatch)
+    user_authorization: User = session.query(User).filter(User.email == user.get('email')).first()
+
+    response = client.get("/api/users/me/",
+            headers={'username': user.get('email'),
+                     'password': user.get('password')},
+        )
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["username"] == user.get('username')
+    assert data["email"] == user.get('email')
+    assert data["password"] == user.get('password')
+
+
+def test_read_users_me_tate_limit(client, user, session, monkeypatch):
+    login_user_token_created(client, session, user, monkeypatch)
+    user_authorization: User = session.query(User).filter(User.email == user.get('email')).first()
+
+    for _ in range(rate_limit.times + 1):
+        response = client.get("/api/users/me/",
+            headers={'username': user.get('email'),
+                     'password': user.get('password')},
+        )
+
+    assert response.status_code == 429, response.text
+    data = response.json()
+    assert data['detail'] == "Too many requests, no more than 10 per minute."
